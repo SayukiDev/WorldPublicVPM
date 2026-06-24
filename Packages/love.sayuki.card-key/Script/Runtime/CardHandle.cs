20978ab5a6d91d6a6905982e8178c3803788e649
さゆき <161200388+SayukiDev@users.noreply.github.com>
2026-06-24 17:47:16 +0900

    feat: follow to player

--- Packages/love.sayuki.card-key/Script/Runtime/CardHandle.cs
+++ Packages/love.sayuki.card-key/Script/Runtime/CardHandle.cs
@@ -10,15 +10,16 @@ namespace love.sayuki.CardKey.Script.Runtime
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
     public class CardHandle : UdonSharpBehaviour
-    { 
+    {
         public int CardID;
-        public TextMeshPro CardText;
         public Transform TeleportPoint;
+        public bool FollowToPlayer;
+        public TextMeshPro CardText;
         public TeleportHandle teleportHandle;
         public ScanDeviceHandle scanDeviceHandle;
         private VRCPlayerApi playerApi;
-        [UdonSynced]
-        private string OwenerName;
+        [UdonSynced] private bool Lock;
+        [UdonSynced] private string OwenerName;
 
         private void Start()
         {
@@ -31,14 +32,24 @@ namespace love.sayuki.CardKey.Script.Runtime
             if (p == null) return;
             if (p.isLocal)
             {
-                /*if (OwenerName != "" && 
+                if (Lock&&
+                    OwenerName != "" &&
                     OwenerName != Networking.LocalPlayer.displayName)
                 {
                     gameObject.GetComponent<VRC_Pickup>().Drop();
                     return;
-                }*/
+                }
+                if (Lock)
+                {
+                    Lock = false;
+                    RequestSerialization();
+                }
+                if (OwenerName == ""&&FollowToPlayer)
+                {
+                    SendCustomEventDelayedSeconds("CheckDistance",5);
+                }
                 OwenerName = p.displayName;
-                 if (!p.isLocal) return;
+                if (!p.isLocal) return;
                 playerApi = p;
                 p.SetPlayerTag(Utils.UserTag, CardID.ToString());
             }
@@ -64,22 +75,68 @@ namespace love.sayuki.CardKey.Script.Runtime
             if (!player.isLocal) return;
             if (OwenerName == player.displayName)
             {
-                Vector3 playerPos = player.GetPosition();
-                Quaternion playerRot = player.GetRotation();
-                Vector3 forwardDirection = playerRot * Vector3.forward;
-                float distance = 0.5f;
-                Vector3 spawnPosition = playerPos + (forwardDirection * distance);
-                Quaternion spawnRotation = playerRot;
-                var gs = gameObject.GetComponent<VRCObjectSync>();
-                if (gs == null)
-                {
-                    return;
-                }
                 Networking.SetOwner(Networking.LocalPlayer, gameObject);
-                gameObject.transform.position = spawnPosition;
-                gameObject.transform.rotation = spawnRotation;
+                if (!Lock)
+                {
+                    Lock = true;
+                }
+                RespawnToPlayer(player,true);
             }
+        }
+
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            if (!Networking.IsOwner(gameObject)) return;
+            if (OwenerName != player.displayName)
+            {
+                return;
+            }
+            Lock = true;
+            RequestSerialization();
+        }
+
+        private void RespawnToPlayer(VRCPlayerApi player, bool forward = false)
+        {
+            Vector3 playerPos = player.GetPosition();
+            Quaternion playerRot = player.GetRotation();
+            Vector3 direction;
+            Quaternion spawnRotation = playerRot;
+            if (forward)
+            {
+                direction = playerRot * Vector3.forward;
+            }
+            else
+            {
+                direction = playerRot * Vector3.back;
+                spawnRotation = Quaternion.Euler(0, 180, 0);
+            }
+            float distance = 0.5f;
+            Vector3 spawnPosition = playerPos + (direction * distance);
+            var gs = gameObject.GetComponent<VRCObjectSync>();
+            if (gs == null)
+            {
+                return;
+            }
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            float h = (float)(player.GetAvatarEyeHeightAsMeters() - 0.1);
+            if (h < 0) h = 0;
+            spawnPosition.y = h;
+            gameObject.transform.position = spawnPosition;
+            gameObject.transform.rotation = spawnRotation;
+            gs.TeleportTo(gameObject.transform);
+        }
+
+        public void CheckDistance()
+        {
+            if (Networking.LocalPlayer.displayName != OwenerName)
+            {
+                return;
+            }
+            if (Vector3.Distance(Networking.LocalPlayer.GetPosition(), gameObject.transform.position) > 3)
+            {
+                RespawnToPlayer(Networking.LocalPlayer, false);
+            }
+            SendCustomEventDelayedSeconds("CheckDistance",5);
         }
     }
 }
-
