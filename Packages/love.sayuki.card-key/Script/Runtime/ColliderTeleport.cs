e9d48097457c234d65dc983ab4ce4901e494adfb
さゆき <161200388+SayukiDev@users.noreply.github.com>
2026-06-26 17:56:19 +0900

    feat: from other handle

--- Packages/love.sayuki.card-key/Script/Runtime/ColliderTeleport.cs
+++ Packages/love.sayuki.card-key/Script/Runtime/ColliderTeleport.cs
@@ -1,3 +1,4 @@
+using love.sayuki.CardKey.Script.Runtime;
 using love.sayuki.CardKey.Script.Utils;
 using UdonSharp;
 using UnityEngine;
@@ -11,8 +12,11 @@ namespace Script.Runtime
         public TeleportHandle teleportHandle;
         public GameObject[] toActivate;
         public GameObject[] toDeactivate;
+        public GameObject[] fromOtherHandle;
+        public bool revertFromOtherHandle;
         private void Start()
         {
+            AddListFromOtherHandle();
             foreach (var t in toActivate)
             {
                 t.SetActive(false);
@@ -21,6 +25,51 @@ namespace Script.Runtime
             {
                 t.SetActive(true);
             }
+            fromOtherHandle = new GameObject[0];
+        }
+
+        private void AddListFromOtherHandle()
+        {
+            foreach (var o in fromOtherHandle)
+            {
+                // card handle
+                var c = o.GetComponent<CardHandle>();
+                if (c != null)
+                {
+                    if (revertFromOtherHandle)
+                    {
+                        toDeactivate = AddToList(toDeactivate,c.toActivate);
+                        toActivate = AddToList(toActivate,c.toDeactivate);
+                    }else{
+                        toActivate = AddToList(toActivate,c.toActivate);
+                        toDeactivate = AddToList(toDeactivate,c.toDeactivate);
+                    }
+                }
+                // staff handle
+                var s=o.GetComponent<ScanDeviceStaffHandle>();
+                if (s != null)
+                {
+                    if (revertFromOtherHandle)
+                    {
+                        toDeactivate = AddToList(toDeactivate,s.toActivate);
+                        toActivate = AddToList(toActivate,s.toDeactivate);
+                    }
+                    else
+                    {
+                        toActivate = AddToList(toActivate,s.toActivate);
+                        toDeactivate = AddToList(toDeactivate,s.toDeactivate);
+                    }
+                }
+            }
+        }
+
+        public GameObject[] AddToList(GameObject[] baseL,GameObject[] toAdd)
+        {
+            var lenght = baseL.Length + toAdd.Length;
+            var tempA=new GameObject[baseL.Length+lenght];
+            baseL.CopyTo(tempA,0);
+            toAdd.CopyTo(tempA,baseL.Length);
+            return tempA;
         }
         
         public override void Interact()
